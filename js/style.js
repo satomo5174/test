df0f3bad1e8284c1af6c9f23a9dee4700f2cec42
Unknown <mail.address.000@gmail.com>
2018-03-12 07:36:23 +0900

    test01

---
+++ js/style.js
@@ -0,0 +1,13 @@
+$(function () { $('.txt').textillate({
+  //繰り返し true or false loop: true,
+  // アニメーションの間隔時間 minDisplayTime: 3000,
+  // アニメーション開始までの遅延時間 initialDelay: 1000,
+  // アニメーションの自動スタート true or false autoStart: true,
+  // 開始時のアニメーション詳細設定 in: {
+  // エフェクト(アニメーション)の指定(デモページにある名称を指定） effect: 'fadeInLeftBig',
+  // 遅延時間の指数 delayScale: 1.5, // 文字ごとの遅延時間 delay: 50, 
+  // true:アニメーションをすべての文字に同時適用 sync: false,
+  // true:文字表示がランダムな順に表示される shuffle: false },
+  // 終了時のアニメーション詳細設定 out: { effect: 'hinge', delayScale: 1.5, delay: 50, sync: false, shuffle: false, }
+   });
+})
